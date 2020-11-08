a746ce7f4f7ac4d8eef2ff857ac056dc93f050f4
Ray Jui-Chien Lin <nturay0321@gmail.com>
2020-11-08 02:12:04 -0500

    ItemConverter with Java 8 stream

--- MyGolf/src/edu/osu/cse5234/converter/ItemConverter.java
+++ MyGolf/src/edu/osu/cse5234/converter/ItemConverter.java
@@ -1,50 +1,44 @@
 package edu.osu.cse5234.converter;
 
-import java.util.ArrayList;
 import java.util.List;
-
+import java.util.stream.Collectors;
 import javax.ejb.LocalBean;
 import javax.ejb.Stateless;
-
 import edu.osu.cse5234.model.Item;
 import edu.osu.cse5234.model.LineItem;
+import lombok.NonNull;
 
 @Stateless
 @LocalBean
 public class ItemConverter {
-	public Item toItem(LineItem lineItem) {
-		Item item = new Item();
-		item.setId(lineItem.getId());		
-		item.setName(lineItem.getItemName());
-		item.setAvailableQuantity(lineItem.getQuantity());
-		item.setItemNumber(lineItem.getItemNumber());
-		item.setUnitPrice(lineItem.getPrice());
-		return item;
+	public Item toDomainObject(@NonNull LineItem dto) {
+		Item domain = new Item();
+		domain.setName(dto.getItemName());
+		domain.setAvailableQuantity(dto.getQuantity());
+		domain.setItemNumber(dto.getItemNumber());
+		domain.setUnitPrice(dto.getPrice());
+		return domain;
 	} 
     
-	public List<Item> toItems(List<LineItem> lineItems) {
-		List<Item> items = new ArrayList<>();
-		for (LineItem lineItem : lineItems) {
-			items.add(toItem(lineItem));
-		}
-		return items;
+	public List<Item> toDomainObjects(@NonNull List<LineItem> dtos) {
+		return dtos.stream()
+				.map(this::toDomainObject)
+				.collect(Collectors.toList());
 	}
 	
-	public LineItem toLineItem(Item item) {
-		LineItem lineItem = new LineItem();
-		lineItem.setId(item.getId());
-		lineItem.setItemName(item.getName());
-		lineItem.setItemNumber(item.getItemNumber());
-		lineItem.setPrice(item.getUnitPrice());
-		lineItem.setQuantity(item.getAvailableQuantity());
-		return lineItem;
+	public LineItem toDataTransferObject(@NonNull Item domain) {
+		LineItem dto = new LineItem();
+		dto.setId(domain.getId());
+		dto.setItemName(domain.getName());
+		dto.setItemNumber(domain.getItemNumber());
+		dto.setPrice(domain.getUnitPrice());
+		dto.setQuantity(domain.getAvailableQuantity());
+		return dto;
 	} 
     
-	public List<LineItem> toLineItems(List<Item> items) {
-		List<LineItem> lineItems = new ArrayList<>();
-		for (Item item : items) {
-			lineItems.add(toLineItem(item));
-		}
-		return lineItems;
+	public List<LineItem> toDataTransferObjects(@NonNull List<Item> domains) {		
+		return domains.stream()
+				.map(this::toDataTransferObject)
+				.collect(Collectors.toList());
 	} 
 }
